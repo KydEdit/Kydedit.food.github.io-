b7773c7b8ccfcad2a43d6c0afdffa12425fba39c
KydEdit <93639961+KydEdit@users.noreply.github.com>
2023-12-01 00:07:54 +0300

    Update forms.js

--- js/modules/forms.js
+++ js/modules/forms.js
@@ -15,18 +15,6 @@ function forms(formSelector, modalTimerId) {
 		bindPostData(item);
 	});
 
-	// const postData = async (url, data) => {
-	// 	const res = await fetch(url, {
-	// 		method: "POST",
-	// 		headers: {
-	// 			'Content-type': 'application/json'
-	// 		},
-	// 		body: data
-	// 	});
-
-	// 	return await res.json();
-	// };
-
 	function bindPostData(form) {
 		form.addEventListener('submit', (e) => {
 			e.preventDefault();
@@ -37,34 +25,13 @@ function forms(formSelector, modalTimerId) {
 				display: block;
 				margin: 0 auto;
 			`;
-			// statusMessage.textContent = message.loading; - удалить
-			// form.append(statusMessage); - удалить
+			
 			form.insertAdjacentElement('afterend', statusMessage);
 
-			// const request = new XMLHttpRequest(); - удалить
-			// request.open('POST', 'server.php'); - удалить
-
-			// request.setRequestHeader('Content-type', 'application/json; charset=utf-8'); - удалить
 			const formData = new FormData(form);
 
 			const json = JSON.stringify(Object.fromEntries(formData.entries()));
-			// const object = {}; - удалить
-			// formData.forEach(function(value, key) { - удалить
-			//         object[key] = value; - удалить
-			// }); - удалить
-			// const json = JSON.stringify(object); - удалить
-			// formData - удалить
-			// request.send(json); - удалить
-
-			// fetch('server.php', { - удалить
-			//         method: "POST", - удалить
-			//         headers: { - удалить
-			//                 'Content-type': 'application/json; charset=utf-8' - удалить
-			//         }, - удалить
-			//         // body: formData - удалить
-			//         body: JSON.stringify(object) - удалить
-			// }) - удалить
-			// JSON.stringify(object)
+		
 			postData('http://localhost:3000/requests', json)
 				// .then(data => data.text()) - удалить 
 				.then(data => {
@@ -77,19 +44,6 @@ function forms(formSelector, modalTimerId) {
 					form.reset();
 				})
 
-			// request.addEventListener('load', () => { - удалить
-			//         if (request.status === 200) { - удалить
-			//                 console.log(request.response); - удалить
-			//                 showThanksModal(message.success); - удалить
-			// statusMessage.textContent = message.success; - удалить
-			// setTimeout(() => { - удалить
-			// statusMessage.remove(); - удалить
-			// }, 2000); - удалить 
-			//                 form.reset(); - удалить
-			//         } else { - удалить
-			//                 showThanksModal(message.failure); - удалить            
-			//         } - удалить
-			// }); - удалить
 		});
 	}
 
