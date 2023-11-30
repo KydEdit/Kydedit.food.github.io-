cefa12aef6b1522f58da5158507bcaaa155c20c1
KydEdit <93639961+KydEdit@users.noreply.github.com>
2023-12-01 00:07:17 +0300

    Update slider.js

--- js/modules/slider.js
+++ js/modules/slider.js
@@ -94,7 +94,7 @@ function slider({container, slide, nextArrow, prevArrow, totalCounter, currentCo
     }
 
     next.addEventListener('click', () => {
-                    // slice(0, width.length - 2) 
+                     
     if (offset == deleteNotDigits(width) * (slides.length - 1)) {
         offset = 0;
     } else {
@@ -109,15 +109,8 @@ function slider({container, slide, nextArrow, prevArrow, totalCounter, currentCo
     }
 
     currentAddZero();
-    // if (slides.length < 10) {
-    // 	current.textContent = `0${slideIndex}`;
-    // } else {
-    // 	current.textContent = slideIndex;
-    // }
 
     dotsOpacity();
-    // dots.forEach(dot => dot.style.opacity = '.5');
-    // dots[slideIndex - 1].style.opacity = 1;
     });
 
     prev.addEventListener('click', () => {
@@ -135,80 +128,19 @@ function slider({container, slide, nextArrow, prevArrow, totalCounter, currentCo
     }
 
     currentAddZero();
-    // if (slides.length < 10) {
-    // 	current.textContent = `0${slideIndex}`;
-    // } else {
-    // 	current.textContent = slideIndex;
-    // }
-
     dotsOpacity();
-    // dots.forEach(dot => dot.style.opacity = '.5');
-    // dots[slideIndex - 1].style.opacity = 1;
     });
-
     dots.forEach(dot => {
     dot.addEventListener('click', (e) => {
         const slideTo = e.target.getAttribute('data-slide-to');
-
         slideIndex = slideTo;
         offset = deleteNotDigits(width) * (slideTo - 1);
-
         slidesField.style.transform = `translateX(-${offset}px)`;
-
+	    
         currentAddZero();
-        // if (slides.length < 10) {
-        // 	current.textContent = `0${slideIndex}`;
-        // } else {
-        // 	current.textContent = slideIndex;
-        // }	
-
         dotsOpacity();
-        // dots.forEach(dot => dot.style.opacity = '.5');
-        // dots[slideIndex - 1].style.opacity = 1;
+   	});
     });
-    });
-
-
-
-    // showSlides(slideIndex);
-
-    // if (slides.length < 10) {
-    // 	total.textContent = `0${slides.length}`;
-    // } else {
-    // 	total.textContent = slides.length;
-    // }
-
-    // function showSlides(n) {
-    // 	if (n > slides.length) {
-    // 		slideIndex = 1;
-    // 	}
-
-    // 	if (n < 1) {
-    // 		slideIndex = slides.length;
-    // 	}
-
-    // 	slides.forEach(item => item.style.display = 'none');
-
-    // 	slides[slideIndex - 1].style.display = 'block';
-
-    // 	if (slides.length < 10) {
-    // 		current.textContent = `0${slideIndex}`;
-    // 	} else {
-    // 		current.textContent = slideIndex;
-    // 	}
-    // }
-
-    // function plusSlides(n) {
-    // 	showSlides(slideIndex += n);
-    // }
-
-    // prev.addEventListener('click', () => {
-    // 	plusSlides(-1);
-    // });
-
-    // next.addEventListener('click', () => {
-    // 	plusSlides(1);
-    // });
 }
 
 export default slider;
